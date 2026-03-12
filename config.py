57b6d903ed55908f88be5a72961d996a3b339c4c
xmovieanime-netizen <xmovieanime@gmail.com>
2026-03-12 15:47:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Limits and IDs
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
-LOGGER_ID = int(getenv("LOGGER_ID", -1002018556839))
-OWNER_ID = int(getenv("OWNER_ID", 6762113050))
+LOGGER_ID = int(getenv("LOGGER_ID", -1003556405442))
+OWNER_ID = int(getenv("OWNER_ID", 7327850684))
 
 # Heroku
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
